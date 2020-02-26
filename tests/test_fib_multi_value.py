3eb9e1ecad2c6994a4bc810faa887ef874a82a3c
Johan Vergeer <johan.vergeer@ordina.nl>
2020-02-26 16:15:20 +0100

    Manually fixed code formatted by Black and added a comment so Black doesn't format it again

--- tests/test_fib_multi_value.py
+++ tests/test_fib_multi_value.py
@@ -6,35 +6,12 @@ from fibonaci.fibonacci import fibonacci_range
 @pytest.mark.parametrize(
     "test_input,expected",
     [
+        # fmt: off
         (0, [0]),
         (4, [0, 1, 1, 2, 3]),
         (10, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]),
-        (
-            20,
-            [
-                0,
-                1,
-                1,
-                2,
-                3,
-                5,
-                8,
-                13,
-                21,
-                34,
-                55,
-                89,
-                144,
-                233,
-                377,
-                610,
-                987,
-                1597,
-                2584,
-                4181,
-                6765,
-            ],
-        ),
+        (20, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]),
+        # fmt: on
     ],
 )
 def test_fibonacci(test_input, expected):
