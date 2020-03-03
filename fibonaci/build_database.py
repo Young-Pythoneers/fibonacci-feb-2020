52e5a624c7fb8f434b626dab17ed9765c3d51632
Nathan Hoogendorp <Nathan.Hoogendorp@ordina.nl>
2020-03-03 16:46:37 +0100

    attempt to fill database

--- fibonaci/build_database.py
+++ fibonaci/build_database.py
@@ -1,5 +1,7 @@
 import os
+
 from config import db
+from fibonacci import fibonacci_recursive
 from models import Fibonacci
 
 # Data to initialize database with
@@ -11,4 +13,20 @@ if os.path.exists("fibonacci.db"):
 # Create the database
 db.create_all()
 
+n = 3000
+for x in range(n):
+    fib_value = Fibonacci(index_value= x, single_value = str(fibonacci_recursive(x)))
+    db.session.add(fib_value)
+
 db.session.commit()
+
+## TEST SCRIPT FOR DB
+import sqlite3
+
+conn = sqlite3.connect('fibonacci.db')
+cur = conn.cursor()
+cur.execute('SELECT * FROM fibonacci')
+fizz_buzz = cur.fetchall()
+print(fizz_buzz)
+# for Fizz in fizz_buzz:
+#     print(Fizz)
