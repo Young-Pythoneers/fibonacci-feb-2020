fc5801e878feaa07750397e52137032530b45739
Johan Vergeer <johan.vergeer@ordina.nl>
2020-03-04 10:54:17 +0100

    Refactor tests

--- tests/test_fib_api.py
+++ tests/test_fib_api.py
@@ -1,18 +1,11 @@
 import pytest
-#from mock import patch
 
-from fibonaci.fib_api import app
-
-def decode_byte(byte):
-    conversion1 = ((byte).decode("ASCII")).replace('\n', '')
-    conversion2 =  conversion1.replace(" ","")
-    return conversion2
+from fibonaci.fib_api import create_app
 
 
 @pytest.fixture
-def client():
-    with app.app.test_client() as client:
-        yield client
+def app():
+    return create_app()
 
 
 def test_app(client):
@@ -20,39 +13,19 @@ def test_app(client):
     assert rv.status_code == 200
 
 
-
 def test_for_index(client):
     rv = client.get("api/fibonacci/for_index?n=20")
     assert rv.status_code == 200
-
-def test_for_index_answer(client):
-    rv = client.get("api/fibonacci/for_index?n=20")
-    assert int(rv.data) == 6765
-
+    assert rv.json == 6765
 
 
 def test_up_to_including_index(client):
-    rv = client.get("/api/fibonacci/up-to-including-index?n=20")
+    rv = client.get("/api/fibonacci/up-to-including-index?n=8")
     assert rv.status_code == 200
-
-def test_up_to_including_index_answer(client):
-    rv = client.get("api/fibonacci/up-to-including-index?n=8")
-    rv2 = decode_byte(rv.data)
-    assert rv2 == '[0,1,1,2,3,5,8,13,21]'
-
+    assert rv.json == [0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 
 def test_up_to_value(client):
     rv = client.get("/api/fibonacci/up-to-value?n=20")
     assert rv.status_code == 200
-
-def test_up_to_value_answer(client):
-    rv = client.get("/api/fibonacci/up-to-value?n=20")
-    rv2 = decode_byte(rv.data)
-    assert rv2 == '[0,1,1,2,3,5,8,13]'
-
-
-
-#@patch('urllib.urlopen')
-#def test_foo(urlopen_mock):
-#    urlopen_mock.return_value = MyUrlOpenMock()
+    assert rv.json == [0, 1, 1, 2, 3, 5, 8, 13]
